bd8c4734a51945b1f5ac9de3f47431a7f8ec45e1
janfriderici <jan@friderici.net>
2022-11-09 11:57:24 +0100

    use flakes to build stuff.
    ... keys seem to be broken.

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     {
       nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
       flake-utils-plus.url = github:gytis-ivaskevicius/flake-utils-plus/master;
-      qmk-nix-utils.url = github:fride/qmk-nix-utils/master;
+      qmk-nix-utils.url = github:fride/qmk-nix-utils/main;
     };
 
   outputs =
@@ -14,41 +14,46 @@
     , flake-utils-plus
     , qmk-nix-utils
     }: flake-utils-plus.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-      qmk-vial-source = pkgs.fetchFromGitHub {
-        owner = "qmk";
-        repo = "qmk_firmware";
-        rev = "0.18.16";
-        sha256 = "sha256-f+IHgUQANDMK0CykfgQ6E2QCB3ZB80RMrlQwhZjwr18=";
-        fetchSubmodules = true;
-      };
-
-      utils-factory = builtins.getAttr system qmk-nix-utils.utils-factory;
-
-      utils = utils-factory
-        {
-          src = ./src;
-          keyboard-name = "ferris";
-          keymap-name = "fride";
-          qmk-firmware-source = qmk-vial-source;
-          flash-script = ''
-            echo -n "Press the RESET button..."
-            while [ ! -e /dev/ttyACM0 ]
-            do
-              echo -n "."
-              sleep 0.5
-            done
-            ${pkgs.avrdude}/bin/avrdude -p atmega32u4 -c avr109 -P /dev/ttyACM0 -U flash:w:$HEX_FILE
-          '';
+        qmk-source = pkgs.fetchFromGitHub {
+          owner = "qmk";
+          repo = "qmk_firmware";
+          rev = "0.18.16";
+          sha256 = "sha256-f+IHgUQANDMK0CykfgQ6E2QCB3ZB80RMrlQwhZjwr18=";
+          fetchSubmodules = true;
         };
-    in
-    {
-      devShell = utils.dev-shell;
-      defaultPackage = utils.hex;
-      defaultApp = utils.flasher;
-      apps.flash = utils.flasher;
-    }
+
+        utils-factory = builtins.getAttr system qmk-nix-utils.utils-factory;
+
+        # I do not like python. Never will!
+        customPython = pkgs.python39.buildEnv.override {
+          extraLibs = [
+            pkgs.python39Packages.pyserial
+            pkgs.python39Packages.pillow];
+        };
+
+        utils = utils-factory
+          {
+            src = ./src;
+            keyboard-name    = "ferris";
+            keymap-name      = "fride";
+            keyboard-version = "/0_2";
+            qmk-firmware-source = qmk-source;
+            extra-build-inputs = [
+              customPython
+            ];
+            flash-script = ''
+              qmk flash -kb ferris/0_2 -km fride
+          '';
+          };
+      in
+        {
+          devShell = utils.dev-shell;
+          defaultPackage = utils.hex;
+          defaultApp = utils.flasher;
+          apps.flash = utils.flasher;
+        }
     );
 }
